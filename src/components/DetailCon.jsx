18db086ad1587134d4dd8a88554633f03a75046f
lee123so <100946922+lee123so@users.noreply.github.com>
2022-08-27 19:39:30 +0900

    Detailpage update

--- src/components/DetailCon.jsx
+++ src/components/DetailCon.jsx
@@ -9,7 +9,8 @@ const DetailCon = () => {
             <DetailBack>이전으로</DetailBack>
         </DetailIdBox>
         <div>
-            <span>수정하기</span>
+            <DetailTit>Title</DetailTit>
+            <DetailDesc>안녕하세요</DetailDesc>
         </div>
     </DetailContainer>
   )
@@ -24,6 +25,7 @@ const DetailContainer = styled.div`
 const DetailIdBox = styled.div`
     display: flex;
     justify-content: space-between;
+    margin-bottom: 30px;
 `
 const DetailId = styled.h1`
     font-size: 24px;
@@ -35,4 +37,11 @@ const DetailBack = styled.div`
     height: 40px;
     text-align: center;
     padding: 5px;
+`
+const DetailTit = styled.h1`
+    font-size: 26px;
+    margin-bottom: 20px;
+`
+const DetailDesc = styled.p`
+    font-size: 16px;
 `
